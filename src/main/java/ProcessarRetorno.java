2a719468003bd25b36ed1bc0f51fc7b4e9f72cc3
Claudio Barbosa <claudiobarebosa@hotmail.com>
2019-09-04 22:17:08 -0300

    implementação dos métodos

---
+++ src/main/java/ProcessarRetorno.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author telecentro
+ */
+public abstract class ProcessarRetorno {
+
+    public abstract void processar(String nomeArquivo);
+
+}
